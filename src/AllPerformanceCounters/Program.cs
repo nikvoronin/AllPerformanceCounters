d06560ae3a0717b251ee81ba9d6eb5e290a13039
nikvoronin <nvoronin@outlook.com>
2022-01-30 01:55:13 +0300

    More functional way

--- src/AllPerformanceCounters/Program.cs
+++ src/AllPerformanceCounters/Program.cs
@@ -1,58 +1,52 @@
 ﻿// NuGet package `System.Diagnostics.PerformanceCounter` have to be installed
+// Less functional way there https://gist.github.com/nikvoronin/643b9dc775c648a0397fdf853f70e7b0
 // .NET 6
 
 #nullable disable
-using System.Globalization;
 using System.Diagnostics;
+using System.Globalization;
+using System.Text;
 
 Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
 using var writer = new StreamWriter( Console.OpenStandardOutput() );
 
 var categories = PerformanceCounterCategory.GetCategories( Environment.MachineName );
-foreach ( var cat in categories ) {
-    writer.WriteLine( $"CATEGORY: {cat.CategoryName}" );
+foreach ( var category in categories ) {
+    writer.WriteLine( $"CATEGORY: {category.CategoryName}" );
 
-    string[] instances = default;
-    try {
-        instances = cat.GetInstanceNames();
-    }
-    catch { }
+    string[] instances = ResultOrDefault( ( cat ) => cat.GetInstanceNames(), category );
 
-    writer.WriteInstances( cat, instances );
+    // fallthrough if there are no instances
+    foreach ( var instanceContent in EnumerateInstances( instances, category ) )
+        writer.Write( instanceContent );
 
     var noInstances = instances.Length == 0;
-    if ( noInstances ) {
-        try {
-            var counters = cat.GetCounters();
-            writer.WriteCounters( counters );
-        }
-        catch { }
-    }
+    if ( noInstances )
+        writer.Write( ResultOrDefault( ( cat ) => cat.GetCounters().ToText(), category ) );
 }
 
 writer.WriteLine( "END." );
 
+static IEnumerable<string> EnumerateInstances( IEnumerable<string> instances, PerformanceCounterCategory category )
+{
+    foreach ( var instance in instances )
+        yield return
+            $"\tINSTANCE: {instance}\n\t"
+            + ResultOrDefault( ( cat ) => cat.GetCounters( instance ), category ).ToText();
+}
+
+static TResult ResultOrDefault<TArg, TResult>( Func<TArg, TResult> f, TArg x )
+{
+    try { return f( x ); }
+    catch { }
+    return default;
+}
+
 public static class Extensions
 {
-    public static void WriteInstances( this StreamWriter writer,
-        PerformanceCounterCategory category,
-        IEnumerable<string> instances )
-    {
-        foreach ( var instanceName in instances ) {
-            try {
-                var counters = category.GetCounters( instanceName );
-                writer.Write( $"\tINSTANCE: {instanceName}\n\t" );
-                writer.WriteCounters( counters );
-            }
-            catch { }
-        }
-    }
-
-    public static void WriteCounters( this StreamWriter writer,
-        IEnumerable<PerformanceCounter> counters )
-    {
-        writer.WriteLine( "\tCOUNTERS:" );
-        foreach ( var counter in counters )
-            writer.WriteLine( $"\t\t{counter.CounterName}" );
-    }
+    public static string ToText( this IEnumerable<PerformanceCounter> counters )
+        => counters?.Aggregate( new StringBuilder( "\tCOUNTERS:\n" ),
+            ( textBuilder, counter ) => textBuilder.AppendLine( $"\t\t{counter.CounterName}" ) )
+        .ToString()
+        ?? string.Empty;
 }
